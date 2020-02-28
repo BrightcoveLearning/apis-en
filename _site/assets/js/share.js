441c290295656ada5bd62211f7a19d9c8408b0a2
Robert Crooks <rcrooks@brightcove.com>
2020-02-28 14:09:49 -0500

    rebuild - shared content fiix

--- _site/assets/js/share.js
+++ _site/assets/js/share.js
@@ -1,14 +1,15 @@
 var BCLS_share = ( function (window, document, BCLS_toc, Prism) {
   document.onload = function() {
-    console.log('here');
-    $(function() {
-      $('#bcls_article').load(
-        'https://general.support.brightcove.com' + shared_content
-      );
-      console.log('page name', shared_content);
-      
-    });
-    BCLS_toc.create_inpage_nav();
-    Prism.highlightAll();
+    if (shared_content) {
+      $(function() {
+        $('#bcls_article').load(
+          'https://general.support.brightcove.com' + shared_content
+        );
+        console.log('page name', shared_content);
+        
+      });
+      BCLS_toc.create_inpage_nav();
+      Prism.highlightAll();
+    }
   };
 })(window, document, BCLS_toc, Prism);
