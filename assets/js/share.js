bb2ebbc04da520ee6aa4371e10ede3f532cd4eba
Robert Crooks <rcrooks@brightcove.com>
2020-03-01 14:20:08 -0500

    fixes

--- assets/js/share.js
+++ assets/js/share.js
@@ -3,14 +3,13 @@ var BCLS_share = ( function (window, document, BCLS_toc, Prism) {
   if (shared_content && shared_content.length > 0) {
   document.onload = function() {
     $(function() {
-      console.log('here');
         $('#bcls_article').load(
           'https://general.support.brightcove.com' + shared_content
         );
       });
-    }
+    };
     // when TOC generator runs, it plants the side_nav_created flag; check not done
     BCLS_toc.create_inpage_nav();
     Prism.highlightAll();
-  };
+  }
 })(window, document, BCLS_toc, Prism);
