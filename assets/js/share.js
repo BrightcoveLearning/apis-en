747c5d65ab1b596a80641df16f5c46a0b4eb5433
Robert Crooks <rcrooks@brightcove.com>
2020-03-01 14:27:02 -0500

    fix typo

--- assets/js/share.js
+++ assets/js/share.js
@@ -2,7 +2,6 @@ var BCLS_share = ( function (window, document, BCLS_toc, Prism) {
   console.log('shared_content', shared_content);
   window.onload = function() {
     console.log('window load');
-    
     $(function() {
         $('#bcls_article').load(
           'https://general.support.brightcove.com' + shared_content
