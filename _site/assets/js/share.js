ddf5c1c799d49f0288a6bcb052181075bb9e69d0
Robert Crooks <rcrooks@brightcove.com>
2020-03-01 14:48:21 -0500

    this should fix it

--- _site/assets/js/share.js
+++ _site/assets/js/share.js
@@ -3,12 +3,11 @@ var BCLS_share = ( function (window, document, BCLS_toc, Prism) {
   window.onload = function() {
     console.log('window load');
     $(function() {
-        $('#bcls_article').load(
-          'https://general.support.brightcove.com' + shared_content
-        );
+        $('#bcls_article').load('https://general.support.brightcove.com' + shared_content, after_content_load);
       });
-    // when TOC generator runs, it plants the side_nav_created flag; check not done
-    BCLS_toc.create_inpage_nav();
-    Prism.highlightAll();
+    function after_content_load() {
+      BCLS_toc.create_inpage_nav();
+      Prism.highlightAll();
+    }
   };
 })(window, document, BCLS_toc, Prism);
