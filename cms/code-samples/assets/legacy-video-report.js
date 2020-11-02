6ea122a2d1281490699656a269dbe654d793209c
Robert Crooks <rcrooks@brightcove.com>
2020-11-02 08:51:08 -0500

    wip

--- cms/code-samples/assets/legacy-video-report.js
+++ cms/code-samples/assets/legacy-video-report.js
@@ -2,42 +2,40 @@ var BCLS = (function (window, document) {
   var account_id,
     client_id,
     client_secret,
+    report_type,
+    initial_offset,
     // api stuff
-    proxyURL =
-      "https://solutions.brightcove.com/bcls/bcls-proxy/bcls-proxy-v2.php",
-    baseURL = "https://cms.api.brightcove.com/v1/accounts/",
-    limit = 25,
-    totalVideos = 0,
-    totalCalls = 0,
-    callNumber = 0,
+    proxyURL        = "https://solutions.brightcove.com/bcls/bcls-proxy/bcls-proxy-v2.php",
+    baseURL         = "https://cms.api.brightcove.com/v1/accounts/",
+    limit           = 25,
+    totalVideos     = 0,
+    totalCalls      = 0,
+    callNumber      = 0,
     videosCompleted = 0,
-    videosArray = [],
-    summaryData = {},
+    videosArray     = [],
+    summaryData     = {},
     csvStr,
     summaryCsvStr,
     customFields = [],
     // elements
-    account_id_element = document.getElementById("account_id"),
-    client_id_element = document.getElementById("client_id"),
-    client_secret_element = document.getElementById("client_secret"),
-    tag = document.getElementById("tag"),
-    videoCount = document.getElementById("videoCount"),
-    makeReport = document.getElementById("makeReport"),
+    account_id_element     = document.getElementById("account_id"),
+    client_id_element      = document.getElementById("client_id"),
+    client_secret_element  = document.getElementById("client_secret"),
+    report_type_element    = document.getElementById('report_type'),
+    initial_offset_element = document.getElementById('initial_offset'),
+    videoCount             = document.getElementById("videoCount"),
+    makeReport             = document.getElementById("makeReport"),
     content,
-    logger = document.getElementById("logger"),
-    logText = document.getElementById("logText"),
-    csvData = document.getElementById("csvData"),
-    apiRequest = document.getElementById("apiRequest"),
-    allButtons = document.getElementsByName("button"),
-    pLogGettingVideos = document.createElement("p"),
+    logger                = document.getElementById("logger"),
+    logText               = document.getElementById("logText"),
+    csvData               = document.getElementById("csvData"),
+    apiRequest            = document.getElementById("apiRequest"),
+    allButtons            = document.getElementsByName("button"),
+    pLogGettingVideos     = document.createElement("p"),
     pLogGettingRenditions = document.createElement("p"),
-    pLogFinish = document.createElement("p"),
-    spanIntro2 = document.createElement("span"),
-    spanOf2 = document.createElement("span"),
-    spanRenditionsTotal = document.createElement("span"),
-    spanRenditionsCount = document.createElement("span"),
-    spanRenditionsTotalEl,
-    spanRenditionsCountEl;
+    pLogFinish            = document.createElement("p"),
+    spanIntro2            = document.createElement("span"),
+    spanOf2               = document.createElement("span");
 
   /**
    * tests for all the ways a variable might be undefined or not have a value
@@ -95,60 +93,6 @@ var BCLS = (function (window, document) {
     }
   }
 
-  /**
-   * sort an array of objects based on an object property
-   * @param {array} targetArray - array to be sorted
-   * @param {string|number} objProperty - object property to sort on
-   * @return sorted array
-   */
-  function sortArray(targetArray, objProperty) {
-    targetArray.sort(function (a, b) {
-      var propA = a[objProperty],
-        propB = b[objProperty];
-      // sort ascending; reverse propA and propB to sort descending
-      if (propA < propB) {
-        return -1;
-      } else if (propA > propB) {
-        return 1;
-      } else {
-        return 0;
-      }
-    });
-    return targetArray;
-  }
-
-  function processRenditions(renditions, callback) {
-    var i,
-      iMax = renditions.length,
-      hlsRenditions = [],
-      mp4Renditions = [],
-      flvRenditions = [],
-      otherRenditions = [],
-      totalSize = 0;
-    // separate renditions by type
-    for (i = 0; i < iMax; i += 1) {
-      if (renditions[i].hasOwnProperty("size")) {
-        totalSize += renditions[i].size;
-      }
-      if (renditions[i].video_container === "M2TS") {
-        hlsRenditions.push(renditions[i]);
-      } else if (renditions[i].video_container === "MP4") {
-        mp4Renditions.push(renditions[i]);
-      } else if (renditions[i].video_container === "FLV") {
-        flvRenditions.push(renditions[i]);
-      } else {
-        otherRenditions.push(renditions[i]);
-      }
-    }
-    // sort renditions by encoding rate
-    callback(
-      hlsRenditions,
-      mp4Renditions,
-      flvRenditions,
-      otherRenditions,
-      totalSize
-    );
-  }
 
   /**
    * determines whether specified item is in an array
@@ -168,37 +112,24 @@ var BCLS = (function (window, document) {
     return false;
   }
 
-  function processCustomFields(fields) {
-    var field;
-    for (field in fields) {
-      if (!arrayContains(customFields, field)) {
-        customFields.push(field);
-      }
-    }
-  }
 
   function startCSVStrings() {
     var i = 0,
       iMax;
-    csvStr =
-      '"ID","Name","Reference ID","Description","Date Added","Date Last Modified","State","Filename","Resolution","Duration(sec)","HLS Renditions (bitrate range KBPS)","MP4 Renditions (bitrate range KBPS)","FLV Renditions (bitrate range KBPS)","Total Rendition Size (MB)",\r\n';
+    csvStr = 
+      '"ID","Name",\r\n';
   }
 
-  function writeReport() {
+  function writeReport(type) {
+    switch (type) {
+      case 'json':
+
+    }
     var i,
       iMax,
       j,
       jMax,
-      video,
-      hlsLowRate,
-      hlsHighRate,
-      mp4LowRate,
-      mp4HighRate,
-      flvLowRate,
-      flvHighRate,
-      resWidth,
-      resHeight,
-      rendition = {};
+      video;
     if (videosArray.length > 0) {
       iMax = videosArray.length;
       for (i = 0; i < iMax; i += 1) {
@@ -207,96 +138,17 @@ var BCLS = (function (window, document) {
         if (video.description) {
           video.description = video.description.replace(/(?:\r\n|\r|\n)/g, " ");
         }
-        // generate the video detail row
-        hlsLowRate =
-          video.hlsRenditions.length > 0
-            ? video.hlsRenditions[0].encoding_rate / 1000
-            : 0;
-        hlsHighRate =
-          video.hlsRenditions.length > 0
-            ? video.hlsRenditions[video.hlsRenditions.length - 1]
-                .encoding_rate / 1000
-            : 0;
-        mp4LowRate =
-          video.mp4Renditions.length > 0
-            ? video.mp4Renditions[0].encoding_rate / 1000
-            : 0;
-        mp4HighRate =
-          video.mp4Renditions.length > 0
-            ? video.mp4Renditions[video.mp4Renditions.length - 1]
-                .encoding_rate / 1000
-            : 0;
-        flvLowRate =
-          video.flvRenditions.length > 0
-            ? video.flvRenditions[0].encoding_rate / 1000
-            : 0;
-        flvHighRate =
-          video.flvRenditions.length > 0
-            ? video.flvRenditions[video.flvRenditions.length - 1]
-                .encoding_rate / 1000
-            : 0;
-        if (video.flvRenditions.length > 0) {
-          rendition = video.flvRenditions[video.flvRenditions.length - 1];
-        } else if (video.mp4Renditions.length > 0) {
-          rendition = video.mp4Renditions[video.mp4Renditions.length - 1];
-        } else if (video.hlsRenditions.length > 0) {
-          rendition = video.hlsRenditions[video.hlsRenditions.length - 1];
-        } else {
-          rendition.frame_width = "unknown";
-          rendition.frame_height = "unknown";
-        }
-        resWidth = rendition.frame_width;
-        resHeight = rendition.frame_height;
         // add csv row
-        csvStr +=
+        csvStr += 
           '"' +
           video.id +
           '","' +
           video.name +
-          '","' +
-          video.reference_id +
-          '","' +
-          video.description +
-          '","' +
-          video.created_at +
-          '","' +
-          video.updated_at +
-          '","' +
-          video.state +
-          '","' +
-          video.original_filename +
-          '","' +
-          resWidth +
-          "x" +
-          resHeight +
-          '","' +
-          video.duration / 1000 +
-          '","' +
-          video.hlsRenditions.length +
-          " (" +
-          hlsLowRate +
-          "-" +
-          hlsHighRate +
-          ')","' +
-          video.mp4Renditions.length +
-          " (" +
-          mp4LowRate +
-          "-" +
-          mp4HighRate +
-          ')","' +
-          video.flvRenditions.length +
-          " (" +
-          flvLowRate +
-          "-" +
-          flvHighRate +
-          ')",' +
-          '"' +
-          video.totalSize / 1000000 +
           '",\r\n';
       }
       csvData.textContent += csvStr;
       // content = document.createTextNode('Finished! See the results or get the CSV data below.');
-      pLogFinish.textContent =
+      pLogFinish.textContent = 
         "Finished! See the results or get the CSV data below.";
       // reportDisplay.innerHTML = summaryReportStr + reportStr;
       enableButtons();
@@ -310,23 +162,23 @@ var BCLS = (function (window, document) {
   function createRequest(id) {
     var endPoint = "",
       parsedData,
-      options = {};
-    options.proxyURL = proxyURL;
-    options.account_id = account_id;
+      options            = {};
+      options.proxyURL   = proxyURL;
+      options.account_id = account_id;
     if (isDefined(client_id) && isDefined(client_secret)) {
-      options.client_id = client_id;
+      options.client_id     = client_id;
       options.client_secret = client_secret;
     }
     // disable buttons to prevent a new request before current one finishes
     disableButtons();
     switch (id) {
-      case "getCount":
+      case "getCount": 
         endPoint = account_id + "/counts/videos?sort=created_at";
         if (isDefined(tag.value)) {
           endPoint += "&q=%2Btags:" + tag.value;
         }
-        options.url = baseURL + endPoint;
-        options.requestType = "GET";
+        options.url            = baseURL + endPoint;
+        options.requestType    = "GET";
         apiRequest.textContent = options.url;
         makeRequest(options, function (response) {
           parsedData = JSON.parse(response);
@@ -337,15 +189,15 @@ var BCLS = (function (window, document) {
           } else {
             totalVideos = totalVideos < video_count ? totalVideos : video_count;
           }
-          totalCalls = Math.ceil(totalVideos / limit);
-          logText.textContent =
+          totalCalls          = Math.ceil(totalVideos / limit);
+          logText.textContent = 
             totalVideos + " videos found; getting account custom fields";
           createRequest("getVideos");
         });
         break;
-      case "getVideos":
-        var offset = limit * callNumber;
-        endPoint =
+      case "getVideos": 
+        var offset   = limit * callNumber;
+            endPoint = 
           account_id +
           "/videos?sort=created_at&limit=" +
           limit +
@@ -354,32 +206,32 @@ var BCLS = (function (window, document) {
         if (isDefined(tag.value)) {
           endPoint += "&q=%2Btags:" + tag.value;
         }
-        options.url = baseURL + endPoint;
-        options.requestType = "GET";
+        options.url            = baseURL + endPoint;
+        options.requestType    = "GET";
         apiRequest.textContent = options.url;
         makeRequest(options, function (response) {
-          parsedData = JSON.parse(response);
+          parsedData  = JSON.parse(response);
           videosArray = videosArray.concat(parsedData);
           callNumber++;
           if (callNumber < totalCalls) {
             createRequest("getVideos");
           } else {
-            callNumber = 0;
+            callNumber                        = 0;
             spanRenditionsCountEl.textContent = callNumber + 1;
             spanRenditionsTotalEl.textContent = totalVideos;
             createRequest("getDigitalMaster");
           }
         });
         break;
-      case "getDigitalMaster":
+      case "getDigitalMaster": 
         videosArray[callNumber].totalSize = 0;
-        endPoint =
+                    endPoint              = 
           account_id +
           "/videos/" +
           videosArray[callNumber].id +
           "/digital_master";
-        options.url = baseURL + endPoint;
-        options.requestType = "GET";
+        options.url            = baseURL + endPoint;
+        options.requestType    = "GET";
         apiRequest.textContent = options.url;
         makeRequest(options, function (response) {
           if (isDefined(response)) {
@@ -398,21 +250,21 @@ var BCLS = (function (window, document) {
           }
         });
         break;
-      case "getVideoRenditions":
+      case "getVideoRenditions": 
         var i,
-          iMax = videosArray.length;
-        videosArray[callNumber].hlsRenditions = [];
-        videosArray[callNumber].mp4Renditions = [];
-        videosArray[callNumber].flvRenditions = [];
-        videosArray[callNumber].otherRenditions = [];
-        endPoint =
+                      iMax                        = videosArray.length;
+          videosArray[callNumber].hlsRenditions   = [];
+          videosArray[callNumber].mp4Renditions   = [];
+          videosArray[callNumber].flvRenditions   = [];
+          videosArray[callNumber].otherRenditions = [];
+                      endPoint                    = 
           account_id +
           "/videos/" +
           videosArray[callNumber].id +
           "/assets/renditions";
-        options.url = baseURL + endPoint;
-        options.requestType = "GET";
-        apiRequest.textContent = options.url;
+        options.url                       = baseURL + endPoint;
+        options.requestType               = "GET";
+        apiRequest.textContent            = options.url;
         spanRenditionsCountEl.textContent = callNumber + 1;
         makeRequest(options, function (response) {
           var renditions = JSON.parse(response);
@@ -449,7 +301,7 @@ var BCLS = (function (window, document) {
             videosArray[callNumber].flvRenditions = [];
           }
           videosCompleted++;
-          logText.textContent =
+          logText.textContent = 
             totalVideos + " videos found; videos retrieved: " + videosCompleted;
           callNumber++;
           if (callNumber < totalVideos) {
@@ -509,7 +361,7 @@ var BCLS = (function (window, document) {
       };
     /**
      * set up request data
-     * the proxy used here takes the following request body:
+     * the proxy used here takes the following request body: 
      * JSON.strinify(options)
      */
     // set response handler
@@ -547,10 +399,10 @@ var BCLS = (function (window, document) {
     // in case of re-run, cleal the results
     csvData.textContent = "";
     // get the inputs
-    client_id = client_id_element.value;
+    client_id     = client_id_element.value;
     client_secret = client_secret_element.value;
-    account_id = account_id_element.value;
-    totalVideos = getSelectedValue(videoCount);
+    account_id    = account_id_element.value;
+    totalVideos   = getSelectedValue(videoCount);
     // only use entered account id if client id and secret are entered also
     if (
       !isDefined(client_id) ||
