c834a71d6467b8f904be26d38b38dc71b1c6d35f
Robert Crooks <rcrooks@brightcove.com>
2022-03-04 14:50:46 -0500

    general-update

--- buildpush.sh
+++ buildpush.sh
@@ -2,7 +2,7 @@ git pull
 bundle update
 bundle exec jekyll build --trace --incremental
 setopt localoptions rmstarsilent
-apis-en git:(master) rm -f _site/feed.xml
+rm -f _site/feed.xml
 git add --all
 git commit --all -m general-update
 git push
