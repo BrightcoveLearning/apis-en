000f875d9012d409be627d440a29e973e1579bf0
Robert Crooks <rcrooks@brightcove.com>
2022-04-14 12:47:58 -0400

    testing

--- buildpush.sh
+++ buildpush.sh
@@ -1,9 +1,11 @@
+bold=`tput bold`
+normal=`tput sgr0`
 git pull
 bundle update
 bundle exec jekyll build --trace --incremental
 setopt localoptions rmstarsilent
 rm -f _site/feed.xml
-echo 'Enter your commit message:'
+echo "Enter your ${bold}COMMIT MESSAGE${normal}:"
 read COMMITMESSAGE
 echo Your client id: $CLIENT_ID
 git add --all
