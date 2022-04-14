78b322b5586193e2cf739a67a0d2be37d7695a50
Robert Crooks <rcrooks@brightcove.com>
2022-04-14 12:43:47 -0400

    Test commit

--- _site/buildpush.sh
+++ _site/buildpush.sh
@@ -3,6 +3,9 @@ bundle update
 bundle exec jekyll build --trace --incremental
 setopt localoptions rmstarsilent
 rm -f _site/feed.xml
+echo 'Enter your commit message:'
+read COMMITMESSAGE
+echo Your client id: $CLIENT_ID
 git add --all
-git commit --all -m general-update
+git commit --all -m $COMMITMESSAGE
 git push
