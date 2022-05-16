65dd4210862afe619bc044cbf991432560348649
Robert Crooks <rcrooks@brightcove.com>
2022-05-16 13:22:30 -0400

    https://brightcove.atlassian.net/browse/KNOW-23742

---
+++ install-submodules.sh
@@ -0,0 +1,24 @@
+git submodule add https://github.com/BrightcoveLearning/s-scripts
+cd _includes
+git submodule add https://github.com/BrightcoveLearning/s-admin
+git submodule add https://github.com/BrightcoveLearning/s-apis-general
+# git submodule add https://github.com/BrightcoveLearning/s-cloudplayout
+# git submodule add https://github.com/BrightcoveLearning/s-social
+# git submodule add https://github.com/BrightcoveLearning/s-players
+# git submodule add https://github.com/BrightcoveLearning/s-publish
+# git submodule add https://github.com/BrightcoveLearning/s-media
+# git submodule add https://github.com/BrightcoveLearning/s-analytics
+git submodule add https://github.com/BrightcoveLearning/s-general
+cd ..
+cd assets/images
+git submodule add https://github.com/BrightcoveLearning/s-admin-img
+git submodule add https://github.com/BrightcoveLearning/s-apis-general-img
+# git submodule add https://github.com/BrightcoveLearning/s-cloudplayout-img
+# git submodule add https://github.com/BrightcoveLearning/s-social-img
+# git submodule add https://github.com/BrightcoveLearning/s-players-img
+# git submodule add https://github.com/BrightcoveLearning/s-publish-img
+# git submodule add https://github.com/BrightcoveLearning/s-media-img
+# git submodule add https://github.com/BrightcoveLearning/s-analytics-img
+git submodule add https://github.com/BrightcoveLearning/s-general-img
+cd ..
+git submodule update --init --recursive
