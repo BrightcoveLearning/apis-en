10c1e72f3531b0a42b2c00a32a627bb07e180f43
Robert Crooks <rcrooks@brightcove.com>
2022-05-22 20:19:31 -0400

    cleaning up

--- _site/install-submodules.sh
+++ _site/install-submodules.sh
@@ -3,24 +3,12 @@ cd _includes
 git submodule add https://github.com/BrightcoveLearning/s-jekyll-page-includes
 git submodule add https://github.com/BrightcoveLearning/s-admin
 git submodule add https://github.com/BrightcoveLearning/s-apis-general
-# git submodule add https://github.com/BrightcoveLearning/s-cloudplayout
-# git submodule add https://github.com/BrightcoveLearning/s-social
-# git submodule add https://github.com/BrightcoveLearning/s-players
-# git submodule add https://github.com/BrightcoveLearning/s-publish
-# git submodule add https://github.com/BrightcoveLearning/s-media
-# git submodule add https://github.com/BrightcoveLearning/s-analytics
 git submodule add https://github.com/BrightcoveLearning/s-general
 git submodule add https://github.com/BrightcoveLearning/s-support
 cd ..
 cd assets/images
 git submodule add https://github.com/BrightcoveLearning/s-admin-img
 git submodule add https://github.com/BrightcoveLearning/s-apis-general-img
-# git submodule add https://github.com/BrightcoveLearning/s-cloudplayout-img
-# git submodule add https://github.com/BrightcoveLearning/s-social-img
-# git submodule add https://github.com/BrightcoveLearning/s-players-img
-# git submodule add https://github.com/BrightcoveLearning/s-publish-img
-# git submodule add https://github.com/BrightcoveLearning/s-media-img
-# git submodule add https://github.com/BrightcoveLearning/s-analytics-img
 git submodule add https://github.com/BrightcoveLearning/s-general-img
 git submodule add https://github.com/BrightcoveLearning/s-support-img
 git submodule add https://github.com/BrightcoveLearning/s-feather-img
