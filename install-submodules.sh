726c92358b317af8f6fa22f46d86aa2a134f4703
Robert Crooks <rcrooks@brightcove.com>
2022-06-12 12:56:29 -0400

    adding misc submodules

--- install-submodules.sh
+++ install-submodules.sh
@@ -5,6 +5,7 @@ git submodule add https://github.com/BrightcoveLearning/s-admin
 git submodule add https://github.com/BrightcoveLearning/s-apis-general
 git submodule add https://github.com/BrightcoveLearning/s-general
 git submodule add https://github.com/BrightcoveLearning/s-support
+git submodule add https://github.com/BrightcoveLearning/s-misc
 cd ..
 cd assets/images
 git submodule add https://github.com/BrightcoveLearning/s-admin-img
@@ -12,6 +13,7 @@ git submodule add https://github.com/BrightcoveLearning/s-apis-general-img
 git submodule add https://github.com/BrightcoveLearning/s-general-img
 git submodule add https://github.com/BrightcoveLearning/s-support-img
 git submodule add https://github.com/BrightcoveLearning/s-feather-img
+git submodule add https://github.com/BrightcoveLearning/s-misc-img
 cd ..
 git submodule add https://github.com/BrightcoveLearning/s-site-assets-favicons
 git submodule add https://github.com/BrightcoveLearning/s-site-assets-stylesheets
