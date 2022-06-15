c02522be07d493b2cf192c864b34ec05b76739be
Robert Crooks <rcrooks@brightcove.com>
2022-06-14 21:21:24 -0400

    adding media submodule

--- _site/install-submodules.sh
+++ _site/install-submodules.sh
@@ -4,6 +4,7 @@ git submodule add https://github.com/BrightcoveLearning/s-jekyll-page-includes
 git submodule add https://github.com/BrightcoveLearning/s-admin
 git submodule add https://github.com/BrightcoveLearning/s-apis-general
 git submodule add https://github.com/BrightcoveLearning/s-general
+git submodule add https://github.com/BrightcoveLearning/s-media
 git submodule add https://github.com/BrightcoveLearning/s-support
 git submodule add https://github.com/BrightcoveLearning/s-misc
 cd ..
@@ -11,6 +12,7 @@ cd assets/images
 git submodule add https://github.com/BrightcoveLearning/s-admin-img
 git submodule add https://github.com/BrightcoveLearning/s-apis-general-img
 git submodule add https://github.com/BrightcoveLearning/s-general-img
+git submodule add https://github.com/BrightcoveLearning/s-media-img
 git submodule add https://github.com/BrightcoveLearning/s-support-img
 git submodule add https://github.com/BrightcoveLearning/s-feather-img
 git submodule add https://github.com/BrightcoveLearning/s-misc-img
