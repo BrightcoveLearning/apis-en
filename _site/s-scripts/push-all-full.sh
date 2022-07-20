81a2dbf1a4db110a43db3d55142659eba7afcd39
Robert <rcrooks@brightcove.com>
2022-07-20 11:18:37 -0400

    Test Build

--- _site/s-scripts/push-all-full.sh
+++ _site/s-scripts/push-all-full.sh
@@ -1,13 +1,14 @@
-echo "Enter your ${bold}COMMIT MESSAGE${normal}:"
-read COMMITMESSAGE 
+bold=`tput bold`
+normal=`tput sgr0`
+COMMITMESSAGE='Test Build'
+# echo "Enter your ${bold}COMMIT MESSAGE${normal}:"
+# read COMMITMESSAGE 
 cd ..
 git pull
 bundle update
 bundle exec jekyll build --trace
 setopt localoptions rmstarsilent
 rm -f _site/feed.xml
-bold=`tput bold`
-normal=`tput sgr0`
 git pull
 git add .
 git commit -m $COMMITMESSAGE
